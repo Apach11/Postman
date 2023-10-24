c5e99c9d724f65208aa80842af07f5bcac931795
Apach <pavel_andreev@inbox.ru>
2023-10-24 11:44:45 +0400

    right

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -17,7 +17,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("Пока"));
+                .body("data",equalTo("Привет"));
     }
 
 }
